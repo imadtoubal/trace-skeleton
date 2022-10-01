5936104e215566a3657dda8c841f9e2129db4523
Imad Toubal <imad.toubal@gmail.com>
2022-10-01 00:56:56 -0500

    Package with setuptools.

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
+import os
+import sys
 
 from setuptools import Extension, setup
 
-example_module = Extension(
-  '_trace_skeleton', sources=['trace_skeleton_wrap.cxx', 'src/trace_skeleton.c'])
+# set the compiler flags so it'll build on different platforms (feel free
+# to file a  pull request with a fix if it doesn't work on yours)
+if sys.platform == 'darwin':
+    # default to clang++ as this is most likely to have c++11 support on OSX
+    if "CC" not in os.environ or os.environ["CC"] == "":
+        os.environ["CC"] = "clang++"
+        # we need to set the min os x version for clang to be okay with
+        # letting us use c++11; also, we don't use dynamic_cast<>, so
+        # we can compile without RTTI to avoid its overhead
+        extra_args = ["-O3", "-stdlib=libc++",
+          "-mmacosx-version-min=10.7","-fno-rtti",
+          "-std=c++0x"]  # c++11
+else: # only tested on travis ci linux servers
+    os.environ["CC"] = "g++" # force compiling c as c++
+    extra_args = ["-O3", '-std=c++0x','-fno-rtti']
+
+_trace_skeleton = Extension(
+  '_trace_skeleton',
+  ['trace_skeleton.i', 'trace_skeleton.c'],
+  extra_compile_args=extra_args,
+)
 
 setup(name='trace_skeleton',
       version='0.1',
       author="SWIG Docs",
       description="""Simple swig example from docs""",
-      ext_modules=[example_module],
-      py_modules=["example"])
+      ext_modules=[_trace_skeleton],
+      py_modules=["trace_skeleton"])
---
+++ test.py
@@ -0,0 +1,18 @@
+import random
+
+import cv2
+
+import trace_skeleton
+
+im = cv2.imread("../test_images/opencv-thinning-src-img.png",0)
+
+_,im = cv2.threshold(im,128,255,cv2.THRESH_BINARY);
+
+polys = trace_skeleton.from_numpy(im);
+
+for l in polys:
+	c = (200*random.random(),200*random.random(),200*random.random())
+	for i in range(0,len(l)-1):
+		cv2.line(im,(l[i][0],l[i][1]),(l[i+1][0],l[i+1][1]),c)
+
+cv2.imshow('',im);cv2.waitKey(0)
